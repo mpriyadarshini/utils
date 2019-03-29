f3315a2de2893f6bbe8226e990a520b8f075d268
Megha Priyadarshini <meghapriyadarshini68@gmail.com>
2019-03-29 23:21:15 +0530

    New changes

--- java/Delete.java
+++ java/Delete.java
@@ -1,15 +1,16 @@
 import java.io.*;
 import java.nio.file.*;
 
-
-
-public class Delete { 
+public class Delete {
     public static void main(String[] args) {
-        Path path = Paths.get(args[0]);
-       try{
-           Files.delete(path);
-       } catch(IOException e){
-           System.err.println(e.getMessage());
-       }
+
+        for (String p : args) {
+            Path path = Paths.get(p);
+            try {
+                Files.delete(path);
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+            }
+        }
     }
 }
